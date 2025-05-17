40618793f4776f8645311c7dd70e77e7e66472b8
veeresh-biradar-tech <biradarveeresh193@gmail.com>
2025-05-17 13:24:03 +0530

    performed conditional statements callled if-else inn simple.js file of simple branch

--- simple.js
+++ simple.js
@@ -4,6 +4,10 @@ let b= 20
 console.log(a)
 console.log(b)
 
-// Arithmetic Operations 
-let result= a+ b
-console.log("Result =", result)
+if(a< b){
+    console.log(a, "is smaller then", b)
+}
+
+else{
+  console.log(a, "is larger then", b)
+}
