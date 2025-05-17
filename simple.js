4d40e72e6a7bb9c30fdb5505ed54efd79ce3a97d
veeresh-biradar-tech <biradarveeresh193@gmail.com>
2025-05-17 13:19:58 +0530

    performing arithmetic operations in simple.js file of main branch

--- simple.js
+++ simple.js
@@ -7,3 +7,12 @@ console.log(b)
 // Arithmetic Operations 
 let result= a+ b
 console.log("Result =", result)
+
+let sub= a-b
+console.log("sub is ", sub)
+
+let multi= a* b
+console.log("Multiple =", multi)
+
+let div= a/ b
+console.log("Division= ", div)
